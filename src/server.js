3522e46f43d955cfc702b5fd82c8f20965fb498a
retia0804 <regame0804@gmail.com>
2023-12-30 12:57:39 +0900

    Add Router

--- src/server.js
+++ src/server.js
@@ -1,14 +1,36 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
 
+const logger = morgan("dev");
+app.use(logger);
+
 // ==========================================================
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const videoRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+videoRouter.get("/", handleEditUser);
+
+const userRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+userRouter.get("/", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+// ==========================================================
+
+// const logger = (req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// };
 
 const privateMiddleware = (req, res, next) => {
   const url = req.url;
@@ -18,7 +40,6 @@ const privateMiddleware = (req, res, next) => {
   next();
 };
 
-app.use(logger);
 app.use(privateMiddleware);
 
 // ==========================================================
